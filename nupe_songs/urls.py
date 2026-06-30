ae13185d59faed84f4a9ddb20ef67e1150faabc4
m16-omar <m16-omar@users.noreply.github.com>
2026-06-30 09:28:38 +0100

    Fix production media routing using Django static serve

--- nupe_songs/urls.py
+++ nupe_songs/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from api.views import (
     landing_view, admin_lyrics_view, admin_favorites_view,
     admin_analytics_view, admin_settings_view, admin_backups_view,
@@ -20,7 +20,9 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-# Serve static/media files (if S3 is not active)
+# Serve static/media files in both development and production (if S3 is not active)
 if not getattr(settings, 'USE_S3', False):
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
