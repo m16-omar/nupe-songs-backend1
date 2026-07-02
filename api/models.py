3261e4a56a84a1acd52a2a7f37fae8923ccf0bb9
m16-omar <m16-omar@users.noreply.github.com>
2026-07-02 12:32:11 +0100

    fix: remove duplicate CustomUserAdmin definition in models.py causing build failure

--- api/models.py
+++ api/models.py
@@ -59,14 +59,6 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.get_full_name() or self.username
 
-@admin.register(CustomUser)
-class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff', 'deleted_at')
-    filter_horizontal = BaseUserAdmin.filter_horizontal + ('favorite_songs',)
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ('Extra Fields', {'fields': ('favorite_songs', 'last_active_at', 'deleted_at')}),
-    )
-
 class Playlist(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="playlists")
